161b61591d7fab9023e715201e1c1aa03e2884e1
wesleyjw <jwesleybiologo@gmail.com>
2022-06-13 14:30:12 -0300

    Add assets and styles css to dash layout

--- DashBoards/review_dashboard/app_dash1.py
+++ DashBoards/review_dashboard/app_dash1.py
@@ -1,12 +1,28 @@
 # Import libs
 from dash import Dash
-from dash_html_components import H1, H2
+import dash_bootstrap_components as dbc
+from dash.html import H1, H2, Div, P
 
 # Instanciar o aplicativo dash
-app = Dash(__name__)
+
+
+# Criando um alista de extilos externos
+external_stylesheets = [
+    'https://unpkg.com/terminal.css@0.7.2/dist/terminal.min.css'
+]
+app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
 # Colocando um titulo
-app.layout = H1("Hello World Dash")
+app.layout = Div(
+    children=[
+        H1("Hello world Dash"),
+        P("Layout para composição de gráfico"),
+        H2("Gráfico 1"),
+        P("Coloque o gráfico aqui"),
+        H2("Gráfico 2"),
+        P("Coloque o gráfico aqui")
+    ]
+)
 
 # Executando o aplicativo
-app.run_server()
+app.run_server(debug=True)
