14632b757556878df56951461a9f590910e816ba
wesleyjw <jwesleybiologo@gmail.com>
2022-06-14 01:00:02 -0300

    Adciona codigo de grafico em tempo real no dash

--- DashBoards/review_dashboard/app_dash1.py
+++ DashBoards/review_dashboard/app_dash1.py
@@ -2,16 +2,35 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
 from dash.html import H1, H2, Div, P
-from dash.dcc import Graph
+from dash.dcc import Graph, Dropdown, Checklist, Interval
+from dash.dependencies import Input, Output
 
-# Instanciar o aplicativo dash
-
+from random import randint
 
 # Criando um alista de extilos externos
 external_stylesheets = [
     'https://unpkg.com/terminal.css@0.7.2/dist/terminal.min.css'
 ]
+# Instanciar o aplicativo dash
 app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+
+# Dataset para os graficos
+# Numero de lementos
+N = 20
+dataset = {
+    'index': list(range(N)),
+    'adults': [randint(18, 100) for _ in range(N)],
+    'kids': [randint(10, 14) for _ in range(N)],
+    'teenagers': [randint(15, 17) for _ in range(N)]
+}
+
+database = {
+    'index': [],
+    'maiores': [],
+    'menores': [],
+    'bebes': [],
+}
+
 
 # Colocando um titulo
 app.layout = Div(
@@ -43,36 +62,148 @@ app.layout = Div(
 
             }
         ),
-        H2("Gráfico 2"),
-        Graph(
-            # Remove a barra lateral padrao no grafico
-            config={'displayModeBar': False},
-            figure={
-                'data': [
-                    {'x': [1, 2, 2, 3, 3, 3, 4, 4, 5],
-                     'type': 'histogram'
-                     }
-                ]
-            }
+        H2("Gráfico Com Dropdown e Call Back"),  # Grafico 2
+        Dropdown(
+            id="drop1",
+            options=[
+                {'label': 'Kids', 'value': 'kids'},
+                {'label': 'Teenagers', 'value': 'teenagers'},
+                {'label': 'Adults', 'value': 'adults'}
+            ],
+            value='adults'
         ),
-        H2("Gráfico 3"),
         Graph(
+            id="graph_call1",
             # Remove a barra lateral padrao no grafico
             config={'displayModeBar': False},
-            figure={
-                'data': [
-                    {'x': [1, 2, 3], 'y': [4, 1, 2],
-                        'type': 'bar', 'name': 'SF'},
-                    {'x': [1, 2, 3], 'y': [2, 4, 5],
-                        'type': 'bar', 'name': u'Montréal'},
-                ],
-                'layout': {
-                    'title': 'Dash Data Visualization'
-                }
-            }
+        ),
+        H2("Gráfico com dois modos interativos"),    # Grafico 3
+        Checklist(
+            id="check1",
+            options=[
+                {'label': 'Adults', 'value': 'adults'},
+                {'label': 'Teenagers', 'value': 'teenagers'},
+                {'label': 'Kids', 'value': 'kids'}
+            ],
+            value=['kids', 'adults', 'teenagers'],
+            inline=True
+        ),
+        Dropdown(
+            id="drop2",
+            options=[
+                {'label': 'Linha', 'value': 'line'},
+                {'label': 'Barras', 'value': 'bar'}
+            ],
+            value='bar'
+        ),
+        Graph(
+            id="graphic_call2",
+            # Remove a barra lateral padrao no grafico
+            config={'displayModeBar': False},
+        ),
+        # Grafico atualizando em tempo real
+        H2("Gráfico em Tempo Real"),
+        Interval(id="interval"),
+        Checklist(
+            id='meu_check_list',
+            options=[
+                {'label': 'Menores de Idade', 'value': 'menores'},
+                {'label': 'Bebes', 'value': 'bebes'},
+                {'label': 'Maiores de idade', 'value': 'maiores'}
+            ],
+            value=['bebes']
+        ),
+        Dropdown(
+            id='meu_dropdown',
+            options=[
+                {'label': 'Linha', 'value': 'line'},
+                {'label': 'Barra', 'value': 'bar'},
+            ],
+            value='bar'
+        ),
+        Graph(
+            id='meu_grafico',
+            config={'displayModeBar': False},
         )
     ]
 )
 
-# Executando o aplicativo
+# Estrutura de Calback para o Grafico 2
+
+
+@app.callback(
+    Output('graph_call1', 'figure'),
+    Input('drop1', 'value')
+)
+def graph1_call(data_input):
+    return {
+        'data': [
+            {'y': dataset[data_input],
+             'type': 'line'
+             }
+        ]
+    }
+
+# Call back grafico 3
+
+
+@app.callback(
+    Output('graphic_call2', 'figure'),
+    [
+        Input('check1', 'value'),
+        Input('drop2', 'value')
+    ]
+)
+def graph2_call(data_input, type_graph):
+    grafico = {
+        'data': [],
+        'layout': {
+            'title': "Dash visualization"
+        }
+    }
+    for x in data_input:
+        grafico['data'].append({
+            'y': dataset[x],
+            'x': dataset['index'],
+            'name': x,
+            'type': type_graph
+        })
+    return grafico
+
+# Grafico em tempo real
+
+
+def update_database(value):
+    """Minha query / Atualização do pandas."""
+    database['index'].append(value)
+    database['menores'].append(randint(1, 200))
+    database['maiores'].append(randint(1, 200))
+    database['bebes'].append(randint(1, 200))
+
+
+@app.callback(
+    Output('meu_grafico', 'figure'),
+    [
+        Input('meu_check_list', 'value'),
+        Input('meu_dropdown', 'value'),
+        Input('interval', 'n_intervals'),
+    ]
+)
+def my_callback(input_data, graph_type, n_intervals):
+    update_database(n_intervals)
+    grafico = {
+        'data': []
+    }
+    for x in input_data:
+        grafico['data'].append(
+            {
+                'y': database[x][-20:],
+                'x': database['index'][-20:],
+                'name': x,
+                'type': graph_type
+            },
+        )
+    return grafico
+
+    # Executando o aplicativo
 app.run_server(debug=True)
