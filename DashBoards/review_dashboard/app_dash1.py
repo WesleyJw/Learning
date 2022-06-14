ffe65715630b3b204e3892dd740891bb003f8e39
wesleyjw <jwesleybiologo@gmail.com>
2022-06-13 21:41:27 -0300

    Modifica notebook  pratica com graficos no dash

--- DashBoards/review_dashboard/app_dash1.py
+++ DashBoards/review_dashboard/app_dash1.py
@@ -2,6 +2,7 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
 from dash.html import H1, H2, Div, P
+from dash.dcc import Graph
 
 # Instanciar o aplicativo dash
 
@@ -18,9 +19,58 @@ app.layout = Div(
         H1("Hello world Dash"),
         P("Layout para composição de gráfico"),
         H2("Gráfico 1"),
-        P("Coloque o gráfico aqui"),
+        Graph(
+            figure={
+                'data': [
+                    {'x': [1, 2, 3, 4, 5],
+                     'type': 'box',
+                     'name': 'Variavel'
+                     }
+                ],
+                'layout': {
+                    'title': 'Box-Plot (Gráfico Bigode)',
+                    'plot_bgcolor': '#222225',  # background dentro do grafico
+                    'paper_bgcolor': '#222225',  # background em volta do grafico
+                    'titlefont': {
+                        'size': 25,
+                        'color': '#e8e9ed'
+                    },
+                    'font': {
+                        'size': 16,
+                        'color': '#e8e9ed'
+                    }
+                }
+
+            }
+        ),
         H2("Gráfico 2"),
-        P("Coloque o gráfico aqui")
+        Graph(
+            # Remove a barra lateral padrao no grafico
+            config={'displayModeBar': False},
+            figure={
+                'data': [
+                    {'x': [1, 2, 2, 3, 3, 3, 4, 4, 5],
+                     'type': 'histogram'
+                     }
+                ]
+            }
+        ),
+        H2("Gráfico 3"),
+        Graph(
+            # Remove a barra lateral padrao no grafico
+            config={'displayModeBar': False},
+            figure={
+                'data': [
+                    {'x': [1, 2, 3], 'y': [4, 1, 2],
+                        'type': 'bar', 'name': 'SF'},
+                    {'x': [1, 2, 3], 'y': [2, 4, 5],
+                        'type': 'bar', 'name': u'Montréal'},
+                ],
+                'layout': {
+                    'title': 'Dash Data Visualization'
+                }
+            }
+        )
     ]
 )
 
